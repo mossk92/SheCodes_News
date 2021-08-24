1f9275509072b531b29351428059392ce5495fa1
mossk92 <86653337+mossk92@users.noreply.github.com>
2021-08-24 21:33:52 +1000

    Model Update for linking profile ?????

--- she_codes_news/users/models.py
+++ she_codes_news/users/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
+from django.db.models.signals import post_save
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    pass
 
     def __str__(self):
         return self.username
+
+def create_profile(send, **kwargs):
+    if kwargs['created']:
+        user_profile = CustomUser.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
