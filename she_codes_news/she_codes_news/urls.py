fa8334ed87146a10be9d151b6a1ee8484db39e73
mossk92 <86653337+mossk92@users.noreply.github.com>
2021-08-28 09:15:11 +1000

    Image Fixing

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('news/', include('news.urls')),
@@ -22,3 +24,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')), #/login and /logout
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings)
