b3a784f28b4778eb212113e4d4c254e6cbb1dea2
mossk92 <86653337+mossk92@users.noreply.github.com>
2021-08-29 13:26:35 +1000

    Getting Image to load correctly

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -5,7 +5,7 @@ from .models import NewsStory
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'pub_date', 'content', 'img_url']
+        fields = ['title', 'pub_date', 'content', 'img_upload']
         widgets = {'pub_date': forms.DateInput(format=('%m/%d/%Y'),
         attrs={'class':'form-control', 'placeholder':'Select a date','type':'date'}),
         }
