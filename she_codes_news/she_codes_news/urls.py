579a1f991b05e3cd4260c0f410032ac425b1bc60
mossk92 <86653337+mossk92@users.noreply.github.com>
2021-08-29 16:02:05 +1000

    Fix Heroku not opening create account

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('news.urls')),
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
