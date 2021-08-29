d4fb5a6231c5b8a2a381a31320b8d344b36281aa
mossk92 <86653337+mossk92@users.noreply.github.com>
2021-08-29 14:50:03 +1000

    Update to home page

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -13,7 +13,8 @@ class IndexView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.order_by('-pub_date')[:4]
+        context['latest_stories'] = NewsStory.objects.order_by('-pub_date')[:3]
+        context['remaining_stories'] = NewsStory.objects.order_by('-pub_date')[3:20]
         context['all_stories'] = NewsStory.objects.order_by('-pub_date')
         return context
 
